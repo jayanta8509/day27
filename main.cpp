a0c85eed9fd2303ff65482f2e81f51853cb68d63
Jayanta Roy <93045961+jayanta8509@users.noreply.github.com>
2023-03-08 15:48:49 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Write a program to find the double of the given number without using
+// arithmetic operator
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+  cout << (a << 1) << endl;
+}
